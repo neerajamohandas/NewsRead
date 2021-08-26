812f2afae581dc9da52d2d2286553a10545007af
Neeraja Mohandas <neerajamohandas@gmail.com>
2021-08-26 20:31:54 +0530

    remember credentials.

---
+++ NewsRead/Model/News.swift
@@ -0,0 +1,20 @@
+//
+//  News.swift
+//  NewsRead
+//
+//  Created by Anand on 26/08/21.
+//
+
+import UIKit
+
+class News: NSObject {
+    
+    var author = ""
+    var title = ""
+    var news_description = ""
+    var clickable_url = ""
+    var image_url = ""
+    var publishedAt = ""
+    
+
+}
---
+++ NewsRead/Views/HomeView.swift
@@ -0,0 +1,42 @@
+//
+//  HomeView.swift
+//  NewsRead
+//
+//  Created by Anand on 26/08/21.
+//
+
+import UIKit
+
+class HomeView: UIView {
+    
+    var view_add : UIView!
+
+   
+    required override init(frame: CGRect) {
+        super.init(frame: frame)
+        nibSetup()
+    }
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        nibSetup()
+    }
+    
+    private func nibSetup(){
+        
+        view_add = loadViewFromNib_()
+        view_add.frame = bounds
+        view_add.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        view_add.translatesAutoresizingMaskIntoConstraints = true
+        addSubview(view_add)
+        
+    }
+    
+    private func loadViewFromNib_() -> UIView {
+        let bundle = Bundle(for: type(of: self))
+        let nib = UINib(nibName: String(describing: type(of: self)), bundle: bundle)
+        let nibView = nib.instantiate(withOwner: self, options: nil).first as! UIView
+        return nibView
+    }
+
+
+}
