745b87249791db808d8b2ebc7388cfdae1653375
Neeraja Mohandas <neerajamohandas@gmail.com>
2021-08-26 12:40:53 +0530

    first commit

---
+++ NewsRead/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsRead
+//
+//  Created by Anand on 26/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
