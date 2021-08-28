9eafb0f81ac78167fdf772ff7bd79cc0756cfca3
Neeraja Mohandas <neerajamohandas@gmail.com>
2021-08-28 16:33:03 +0530

    completed designing signup view.

--- NewsRead/Views/SignUPView.swift
+++ NewsRead/Views/SignUPView.swift
@@ -9,6 +9,15 @@ import UIKit
 
 class SignUPView: SuperView {
     
+    @IBOutlet var mdtxtf_fullName: MDTextFieldView!
+    @IBOutlet var mdtxtf_username: MDTextFieldView!
+    @IBOutlet var mdtxtf_email: MDTextFieldView!
+    @IBOutlet var mdtxtf_dob: MDTextFieldView!
+    @IBOutlet var mdtxtf_mobno: MDTextFieldView!
+    @IBOutlet var mdtxtf_password: MDTextFieldView!
+    @IBOutlet var mdtxtf_confirmPassword: MDTextFieldView!
+    @IBOutlet var btn_Signup: UIButton!
+    
     required init(frame: CGRect) {
         super.init(frame: frame)
         setupView()
@@ -21,8 +30,10 @@ class SignUPView: SuperView {
     
     func setupView(){
         
-        
+        btn_Signup.layer.cornerRadius = 5.0
         
     }
     
+    @IBAction func btn_Signup_Clicked(_ sender: UIButton) {
+    }
 }
